c04d939e17942b31e1bc2510c02d88554be1703e
MargaritaPustovalova <71708472+MargaritaPustovalova@users.noreply.github.com>
2021-02-11 17:39:27 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 1800;
+        int percent = 1;
+        int bonus;
+        int limit = 1000;
+        if (amount >= limit) {
+            bonus = amount * percent / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println(bonus);
+    }
+}
